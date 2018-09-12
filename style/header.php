722c9d5d0da2cb874fe0d02a2a19fa8f1768a1cb
JavaGuy12/ITGuy12 <33841260+JavaGuy12@users.noreply.github.com>
2018-09-11 21:31:16 -0400

    Update header.php

--- style/header.php
+++ style/header.php
@@ -1,1 +1,11 @@
-<?php ?>
+<?php 
+   session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" type="text/css" href="style/main.css">
+  </head>
+  <body>
+    <p> Hello </p>
